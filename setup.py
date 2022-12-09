b89834419f76d1601f9b8122a2af0b7f042bde63
Luong-Ha Nguyen <ha@Luong-Has-MBP.air>
2022-12-09 16:22:26 -0500

    Test py310 and 311

--- setup.py
+++ setup.py
@@ -163,6 +163,7 @@ setup(
     install_requires=[
         "numpy"
     ],
+     python_requires=">=3.10",
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
